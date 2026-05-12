928e320119bdb4e0ae3e1adb4d84b75b9809549f
Joel Takvorian <jtakvori@redhat.com>
2026-05-12 10:18:12 +0200

    NETOBSERV-2608: TLS metrics (#2707)
    * NETOBSERV-2608: TLS metrics
    
    Create specific metrics for TLS, counting flow, labelled by:
    - TLSVersion
    - TLSCipherSuite
    - TLSGroup
    - TLSTypes
    - Proto
    
    * refresh bundle, fix tests
    
    * add tls metrics by default
    
    * Remove TLSTypes label from metrics, but use it as a filter instead
    
    * Add monitoring charts for TLS
    
    Also add otel mapping
    
    * Use consistant rate interval in TLS dashboard
    
    * Remove old dead code / leftover from old API
    
    The metrics tagging system is a leftover from old API, and is dead code
    since that API was removed.

--- internal/pkg/metrics/predefined_metrics.go
+++ internal/pkg/metrics/predefined_metrics.go
@@ -2,7 +2,6 @@ package metrics
 
 import (
 	"fmt"
-	"reflect"
 	"slices"
 	"strings"
 
@@ -21,25 +20,16 @@ const (
 var (
 	latencyBuckets = []string{".005", ".01", ".02", ".03", ".04", ".05", ".075", ".1", ".25", "1"}
 	mapLabels      = map[string][]string{
-		tagNodes:      {"K8S_ClusterName", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_HostName", "DstK8S_HostName", "TLSVersion"},
-		tagNamespaces: {"K8S_ClusterName", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_Namespace", "DstK8S_Namespace", "K8S_FlowLayer", "SrcSubnetLabel", "DstSubnetLabel", "TLSVersion"},
-		tagWorkloads:  {"K8S_ClusterName", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_Namespace", "DstK8S_Namespace", "K8S_FlowLayer", "SrcSubnetLabel", "DstSubnetLabel", "SrcK8S_NetworkName", "DstK8S_NetworkName", "SrcK8S_OwnerName", "DstK8S_OwnerName", "SrcK8S_OwnerType", "DstK8S_OwnerType", "SrcK8S_Type", "DstK8S_Type", "TLSVersion"},
+		tagNodes:      {"K8S_ClusterName", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_HostName", "DstK8S_HostName"},
+		tagNamespaces: {"K8S_ClusterName", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_Namespace", "DstK8S_Namespace", "K8S_FlowLayer", "SrcSubnetLabel", "DstSubnetLabel"},
+		tagWorkloads:  {"K8S_ClusterName", "SrcK8S_Zone", "DstK8S_Zone", "SrcK8S_Namespace", "DstK8S_Namespace", "K8S_FlowLayer", "SrcSubnetLabel", "DstSubnetLabel", "SrcK8S_NetworkName", "DstK8S_NetworkName", "SrcK8S_OwnerName", "DstK8S_OwnerName", "SrcK8S_OwnerType", "DstK8S_OwnerType", "SrcK8S_Type", "DstK8S_Type"},
 	}
 	mapValueFields = map[string]string{
 		tagBytes:   "Bytes",
 		tagPackets: "Packets",
 	}
-	predefinedMetrics []taggedMetricDefinition
-	// Pre-deprecation default IgnoreTags list (1.4) - used before switching to whitelist approach,
-	// to make sure there is no unintended new metrics being collected
-	// Don't add anything here: this is not meant to evolve
-	defaultIgnoreTags1_4 = []string{"egress", "packets", "nodes-flows", "namespaces-flows", "workloads-flows", "namespaces"}
+	predefinedMetrics []metricslatest.FlowMetricSpec
 )
-
-type taggedMetricDefinition struct {
-	metricslatest.FlowMetricSpec
-	tags []string
-}
 
 func init() {
 	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
@@ -50,214 +40,169 @@ func init() {
 			valueField := mapValueFields[vt]
 			for _, dir := range []metricslatest.FlowDirection{metricslatest.Egress, metricslatest.Ingress} {
 				lowDir := strings.ToLower(string(dir))
-				predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-					FlowMetricSpec: metricslatest.FlowMetricSpec{
-						MetricName: fmt.Sprintf("%s_%s_%s_total", groupTrimmed, lowDir, vt),
-						Type:       metricslatest.CounterMetric,
-						Help:       fmt.Sprintf("Total %s per %s in %s direction", vt, groupTrimmed, lowDir),
-						ValueField: valueField,
-						Direction:  dir,
-						Labels:     labels,
-						Charts:     trafficCharts(group, vt, lowDir),
-					},
-					tags: []string{group, vt, lowDir},
+				predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+					MetricName: fmt.Sprintf("%s_%s_%s_total", groupTrimmed, lowDir, vt),
+					Type:       metricslatest.CounterMetric,
+					Help:       fmt.Sprintf("Total %s per %s in %s direction", vt, groupTrimmed, lowDir),
+					ValueField: valueField,
+					Direction:  dir,
+					Labels:     labels,
+					Charts:     trafficCharts(group, vt, lowDir),
 				})
 			}
 		}
 		// Sampling
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_sampling", groupTrimmed),
-				Type:       metricslatest.GaugeMetric,
-				Help:       fmt.Sprintf("Sampling per %s", groupTrimmed),
-				ValueField: "Sampling",
-				Labels:     labels,
-			},
-			tags: []string{group, "sampling"},
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_sampling", groupTrimmed),
+			Type:       metricslatest.GaugeMetric,
+			Help:       fmt.Sprintf("Sampling per %s", groupTrimmed),
+			ValueField: "Sampling",
+			Labels:     labels,
 		})
 		// Flows metrics
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_flows_total", groupTrimmed),
-				Type:       "counter",
-				Help:       fmt.Sprintf("Total flows per %s", groupTrimmed),
-				Labels:     labels,
-			},
-			tags: []string{group, group + "-flows", "flows"},
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_flows_total", groupTrimmed),
+			Type:       "counter",
+			Help:       fmt.Sprintf("Total flows per %s", groupTrimmed),
+			Labels:     labels,
 		})
 	}
+	// RTT metrics
 	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
 		groupTrimmed := strings.TrimSuffix(group, "s")
 		labels := mapLabels[group]
-		// RTT metrics
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_rtt_seconds", groupTrimmed),
-				Type:       metricslatest.HistogramMetric,
-				Help:       fmt.Sprintf("Round-trip time latency in seconds per %s", groupTrimmed),
-				ValueField: "TimeFlowRttNs",
-				Filters: []metricslatest.MetricFilter{
-					{Field: "TimeFlowRttNs", MatchType: metricslatest.MatchPresence},
-				},
-				Labels:  labels,
-				Divider: "1000000000", // ns => s
-				Buckets: latencyBuckets,
-				Charts:  rttCharts(group),
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_rtt_seconds", groupTrimmed),
+			Type:       metricslatest.HistogramMetric,
+			Help:       fmt.Sprintf("Round-trip time latency in seconds per %s", groupTrimmed),
+			ValueField: "TimeFlowRttNs",
+			Filters: []metricslatest.MetricFilter{
+				{Field: "TimeFlowRttNs", MatchType: metricslatest.MatchPresence},
 			},
-			tags: []string{group, "rtt"},
+			Labels:  labels,
+			Divider: "1000000000", // ns => s
+			Buckets: latencyBuckets,
+			Charts:  rttCharts(group),
 		})
 	}
+	// Drops metrics
 	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
 		groupTrimmed := strings.TrimSuffix(group, "s")
 		labels := mapLabels[group]
-		// Drops metrics
 		dropLabels := labels
 		dropLabels = append(dropLabels, "PktDropLatestState", "PktDropLatestDropCause")
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_drop_packets_total", groupTrimmed),
-				Type:       metricslatest.CounterMetric,
-				Help:       fmt.Sprintf("Total dropped packets per %s", groupTrimmed),
-				ValueField: "PktDropPackets",
-				Filters: []metricslatest.MetricFilter{
-					{Field: "PktDropPackets", MatchType: metricslatest.MatchPresence},
-				},
-				Labels: dropLabels,
-				Charts: dropCharts(group, "pps"),
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_drop_packets_total", groupTrimmed),
+			Type:       metricslatest.CounterMetric,
+			Help:       fmt.Sprintf("Total dropped packets per %s", groupTrimmed),
+			ValueField: "PktDropPackets",
+			Filters: []metricslatest.MetricFilter{
+				{Field: "PktDropPackets", MatchType: metricslatest.MatchPresence},
 			},
-			tags: []string{group, tagPackets, "drops"},
+			Labels: dropLabels,
+			Charts: dropCharts(group, "pps"),
 		})
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_drop_bytes_total", groupTrimmed),
-				Type:       metricslatest.CounterMetric,
-				Help:       fmt.Sprintf("Total dropped bytes per %s", groupTrimmed),
-				ValueField: "PktDropBytes",
-				Filters: []metricslatest.MetricFilter{
-					{Field: "PktDropBytes", MatchType: metricslatest.MatchPresence},
-				},
-				Labels: dropLabels,
-				Charts: dropCharts(group, "Bps"),
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_drop_bytes_total", groupTrimmed),
+			Type:       metricslatest.CounterMetric,
+			Help:       fmt.Sprintf("Total dropped bytes per %s", groupTrimmed),
+			ValueField: "PktDropBytes",
+			Filters: []metricslatest.MetricFilter{
+				{Field: "PktDropBytes", MatchType: metricslatest.MatchPresence},
 			},
-			tags: []string{group, tagBytes, "drop"},
+			Labels: dropLabels,
+			Charts: dropCharts(group, "Bps"),
 		})
 	}
+	// DNS metrics
 	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
 		groupTrimmed := strings.TrimSuffix(group, "s")
 		labels := mapLabels[group]
-		// DNS metrics
 		dnsLabels := labels
 		dnsLabels = append(dnsLabels, "DnsFlagsResponseCode")
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_dns_latency_seconds", groupTrimmed),
-				Type:       metricslatest.HistogramMetric,
-				Help:       fmt.Sprintf("DNS latency in seconds per %s", groupTrimmed),
-				ValueField: "DnsLatencyMs",
-				Filters: []metricslatest.MetricFilter{
-					{Field: "DnsId", MatchType: metricslatest.MatchPresence},
-				},
-				Labels:  dnsLabels,
-				Divider: "1000", // ms => s
-				Buckets: latencyBuckets,
-				Charts:  dnsCharts(group),
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_dns_latency_seconds", groupTrimmed),
+			Type:       metricslatest.HistogramMetric,
+			Help:       fmt.Sprintf("DNS latency in seconds per %s", groupTrimmed),
+			ValueField: "DnsLatencyMs",
+			Filters: []metricslatest.MetricFilter{
+				{Field: "DnsId", MatchType: metricslatest.MatchPresence},
 			},
-			tags: []string{group, "dns"},
+			Labels:  dnsLabels,
+			Divider: "1000", // ms => s
+			Buckets: latencyBuckets,
+			Charts:  dnsCharts(group),
 		})
 	}
 
+	// Netpol metrics
 	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
 		groupTrimmed := strings.TrimSuffix(group, "s")
 		labels := mapLabels[group]
-		// Netpol metrics
 		netpolLabels := labels
 		netpolLabels = append(netpolLabels, "NetworkEvents>Type", "NetworkEvents>Namespace", "NetworkEvents>Name", "NetworkEvents>Action", "NetworkEvents>Direction")
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_network_policy_events_total", groupTrimmed),
-				Type:       "counter",
-				Help:       fmt.Sprintf("Total network policy events per %s", groupTrimmed),
-				Labels:     netpolLabels,
-				Filters:    []metricslatest.MetricFilter{{Field: "NetworkEvents>Feature", Value: "acl"}},
-				Flatten:    []string{"NetworkEvents"},
-				Remap: map[string]metricslatest.Label{
-					"NetworkEvents>Type":      "type",
-					"NetworkEvents>Namespace": "namespace",
-					"NetworkEvents>Name":      "name",
-					"NetworkEvents>Action":    "action",
-					"NetworkEvents>Direction": "direction",
-				},
-				Charts: netpolCharts(group),
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_network_policy_events_total", groupTrimmed),
+			Type:       "counter",
+			Help:       fmt.Sprintf("Total network policy events per %s", groupTrimmed),
+			Labels:     netpolLabels,
+			Filters:    []metricslatest.MetricFilter{{Field: "NetworkEvents>Feature", Value: "acl"}},
+			Flatten:    []string{"NetworkEvents"},
+			Remap: map[string]metricslatest.Label{
+				"NetworkEvents>Type":      "type",
+				"NetworkEvents>Namespace": "namespace",
+				"NetworkEvents>Name":      "name",
+				"NetworkEvents>Action":    "action",
+				"NetworkEvents>Direction": "direction",
 			},
-			tags: []string{group, "network-policy"},
+			Charts: netpolCharts(group),
 		})
 	}
 
+	// TLS
 	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
 		groupTrimmed := strings.TrimSuffix(group, "s")
 		labels := mapLabels[group]
-		// IPSEC
-		ipsecLabels := labels
-		ipsecLabels = append(ipsecLabels, "IPSecStatus")
-		predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-			FlowMetricSpec: metricslatest.FlowMetricSpec{
-				MetricName: fmt.Sprintf("%s_ipsec_flows_total", groupTrimmed),
-				Type:       metricslatest.CounterMetric,
-				Help:       fmt.Sprintf("Total IPsec encrypted flows per %s", groupTrimmed),
-				Filters:    []metricslatest.MetricFilter{{Field: "IPSecStatus", MatchType: metricslatest.MatchPresence}},
-				Labels:     ipsecLabels,
-				Charts:     ipsecStatusChart(group),
-			},
-			tags: []string{group, "ipsec"},
+		tlsLabels := labels
+		tlsLabels = append(tlsLabels, "TLSVersion", "TLSCipherSuite", "TLSGroup")
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_tls_flows_total", groupTrimmed),
+			Type:       metricslatest.CounterMetric,
+			Help:       fmt.Sprintf("Total TLS flows per %s", groupTrimmed),
+			Filters:    []metricslatest.MetricFilter{{Field: "TLSTypes", MatchType: metricslatest.MatchPresence}},
+			Labels:     tlsLabels,
+			Charts:     tlsStatusChart(),
 		})
 	}
-	// Cross-nodes metric
-	predefinedMetrics = append(predefinedMetrics, taggedMetricDefinition{
-		FlowMetricSpec: metricslatest.FlowMetricSpec{
-			MetricName: "node_to_node_ingress_flows_total",
+
+	// IPSEC
+	for _, group := range []string{tagNodes, tagNamespaces, tagWorkloads} {
+		groupTrimmed := strings.TrimSuffix(group, "s")
+		labels := mapLabels[group]
+		ipsecLabels := labels
+		ipsecLabels = append(ipsecLabels, "IPSecStatus")
+		predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+			MetricName: fmt.Sprintf("%s_ipsec_flows_total", groupTrimmed),
 			Type:       metricslatest.CounterMetric,
-			Help:       "Total ingress flows between nodes",
-			Labels:     mapLabels[tagNodes],
-			Filters: []metricslatest.MetricFilter{
-				{Field: "FlowDirection", Value: "2", MatchType: metricslatest.MatchNotEqual},
-				{Field: "SrcK8S_HostName", MatchType: metricslatest.MatchPresence},
-				{Field: "DstK8S_HostName", MatchType: metricslatest.MatchPresence},
-			},
+			Help:       fmt.Sprintf("Total IPsec encrypted flows per %s", groupTrimmed),
+			Filters:    []metricslatest.MetricFilter{{Field: "IPSecStatus", MatchType: metricslatest.MatchPresence}},
+			Labels:     ipsecLabels,
+			Charts:     ipsecStatusChart(group),
+		})
+	}
+
+	// Cross-nodes metric
+	predefinedMetrics = append(predefinedMetrics, metricslatest.FlowMetricSpec{
+		MetricName: "node_to_node_ingress_flows_total",
+		Type:       metricslatest.CounterMetric,
+		Help:       "Total ingress flows between nodes",
+		Labels:     mapLabels[tagNodes],
+		Filters: []metricslatest.MetricFilter{
+			{Field: "FlowDirection", Value: "2", MatchType: metricslatest.MatchNotEqual},
+			{Field: "SrcK8S_HostName", MatchType: metricslatest.MatchPresence},
+			{Field: "DstK8S_HostName", MatchType: metricslatest.MatchPresence},
 		},
-		tags: []string{tagNodes, "ipsec"},
 	})
-}
-
-func isIgnored(def *taggedMetricDefinition, ignoreTags []string) bool {
-	for _, ignoreTag := range ignoreTags {
-		for _, tag := range def.tags {
-			if ignoreTag == tag {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func convertIgnoreTagsToIncludeList(ignoreTags []string) []flowslatest.FLPMetric {
-	ret := []flowslatest.FLPMetric{}
-	for i := range predefinedMetrics {
-		if !isIgnored(&predefinedMetrics[i], ignoreTags) {
-			ret = append(ret, flowslatest.FLPMetric(predefinedMetrics[i].MetricName))
-		}
-	}
-	return ret
-}
-
-func GetAsIncludeList(ignoreTags []string, includeList *[]flowslatest.FLPMetric) *[]flowslatest.FLPMetric {
-	if includeList == nil && len(ignoreTags) > 0 {
-		if reflect.DeepEqual(ignoreTags, defaultIgnoreTags1_4) {
-			return nil
-		}
-		converted := convertIgnoreTagsToIncludeList(ignoreTags)
-		return &converted
-	}
-	return includeList
 }
 
 func GetAllNames() []string {
@@ -272,7 +217,7 @@ func getUpdatedDefsFromNames(names []string, labelsToRemove []string, filterReco
 	ret := []metricslatest.FlowMetric{}
 	for i := range predefinedMetrics {
 		if slices.Contains(names, predefinedMetrics[i].MetricName) {
-			spec := predefinedMetrics[i].FlowMetricSpec
+			spec := predefinedMetrics[i]
 			spec.Labels = removeLabels(spec.Labels, labelsToRemove)
 			if filterRecordType != nil {
 				spec.Filters = append(spec.Filters, *filterRecordType)
@@ -321,9 +266,6 @@ func GetDefinitions(fc *flowslatest.FlowCollectorSpec, allMetrics bool) []metric
 	}
 	if !fc.Agent.EBPF.IsUDNMappingEnabled() && len(fc.GetSecondaryIndexes()) == 0 {
 		labelsToRemove = append(labelsToRemove, "SrcK8S_NetworkName", "DstK8S_NetworkName")
-	}
-	if !fc.Agent.EBPF.IsTLSTrackingEnabled() {
-		labelsToRemove = append(labelsToRemove, "TLSVersion")
 	}
 
 	var filterRecordType *metricslatest.MetricFilter
